6f477b3836eebc98b0f8f2e18d6740bc8efcee83
johnjunzhang <johnjunzhang@gmail.com>
2014-07-08 17:46:45 +0800

    [Jun] Functions Should Do One Thing

--- practices/src/main/scala/refactoringToObserver/WeatherData.scala
+++ practices/src/main/scala/refactoringToObserver/WeatherData.scala
@@ -4,15 +4,14 @@ import scala.collection.mutable
 
 class WeatherData(seedingMachine: SeedingMachine, reapingMachine: ReapingMachine, wateringMachine: WateringMachine) {
   private val observers = mutable.MutableList[IObserver]()
+  val seedingMachineObserver = new SeedingMachineObserver(seedingMachine)
+  val wateringMachineObserver = new WateringMachineObserver(wateringMachine)
+  val reapingMachineObserver = new ReapingMachineObserver(reapingMachine)
+  observers += seedingMachineObserver
+  observers += wateringMachineObserver
+  observers += reapingMachineObserver
 
   def measurementsChanged(temp: Int, humidity: Int, windPower: Int):Unit = {
-    val seedingMachineObserver = new SeedingMachineObserver(seedingMachine)
-    val wateringMachineObserver = new WateringMachineObserver(wateringMachine)
-    val reapingMachineObserver = new ReapingMachineObserver(reapingMachine)
-    observers += seedingMachineObserver
-    observers += wateringMachineObserver
-    observers += reapingMachineObserver
-
     observers.foreach(_.update(temp, humidity, windPower))
   }
 }
