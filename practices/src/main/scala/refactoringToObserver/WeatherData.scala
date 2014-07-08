13b4e92d7a0663cb31220b4a11869337557eb1db
johnjunzhang <johnjunzhang@gmail.com>
2014-07-08 17:44:18 +0800

    [Jun] Duplicate Code

--- practices/src/main/scala/refactoringToObserver/WeatherData.scala
+++ practices/src/main/scala/refactoringToObserver/WeatherData.scala
@@ -1,24 +1,18 @@
 package refactoringToObserver
 
+import scala.collection.mutable
+
 class WeatherData(seedingMachine: SeedingMachine, reapingMachine: ReapingMachine, wateringMachine: WateringMachine) {
+  private val observers = mutable.MutableList[IObserver]()
+
   def measurementsChanged(temp: Int, humidity: Int, windPower: Int):Unit = {
-    seedingMachineUpdate(temp, humidity, windPower)
-    reapingMachineUpdate(temp, humidity, windPower)
-    wateringMachineUpdate(temp, humidity, windPower)
-  }
-
-  def wateringMachineUpdate(temp: Int, humidity: Int, windPower: Int) {
-    val wateringMachineObserver = new WateringMachineObserver(wateringMachine)
-    wateringMachineObserver.update(temp, humidity, windPower)
-  }
-
-  def reapingMachineUpdate(temp: Int, humidity: Int, windPower:Int) {
-    val reapingMachineObserver = new ReapingMachineObserver(reapingMachine)
-    reapingMachineObserver.update(temp, humidity, windPower)
-  }
-
-  def seedingMachineUpdate(temp: Int, humidity: Int, windPower:Int) {
     val seedingMachineObserver = new SeedingMachineObserver(seedingMachine)
-    seedingMachineObserver.update(temp, humidity, windPower)
+    val wateringMachineObserver = new WateringMachineObserver(wateringMachine)
+    val reapingMachineObserver = new ReapingMachineObserver(reapingMachine)
+    observers += seedingMachineObserver
+    observers += wateringMachineObserver
+    observers += reapingMachineObserver
+
+    observers.foreach(_.update(temp, humidity, windPower))
   }
 }
