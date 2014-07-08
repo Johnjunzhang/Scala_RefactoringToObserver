eadaaaef018ea41740a397b090b8cc281ea5a7ad
johnjunzhang <johnjunzhang@gmail.com>
2014-07-08 17:58:51 +0800

    [Jun] Parameterize clients with different request

--- practices/src/test/scala/refactoringToObserverFacts/WeatherDataFact.scala
+++ practices/src/test/scala/refactoringToObserverFacts/WeatherDataFact.scala
@@ -3,7 +3,7 @@ package movingFeaturesFacts
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, FunSpec}
-import refactoringToObserver.{WateringMachine, ReapingMachine, SeedingMachine, WeatherData}
+import refactoringToObserver._
 
 @RunWith(classOf[JUnitRunner])
 class WeatherDataFact extends FunSpec with Matchers {
@@ -13,25 +13,25 @@ class WeatherDataFact extends FunSpec with Matchers {
 
   describe("WeatherDataFact") {
     it("seeding_machine_should_start_if_temperature_over_5_degree") {
-      val weatherData = getWeatherData
+      val weatherData = new WeatherData(seedingMachine, reapingMachine, wateringMachine)
+      weatherData.subscribe(new SeedingMachineObserver(seedingMachine))
       weatherData.measurementsChanged(10, 0, 0)
       seedingMachine.status should be(true)
     }
 
     it("reaping_machine_should_start_if_temperature_over_5_degree_and_humidity_over_65") {
-      val weatherData = getWeatherData
+      val weatherData = new WeatherData(seedingMachine, reapingMachine, wateringMachine)
+      weatherData.subscribe(new ReapingMachineObserver(reapingMachine))
       weatherData.measurementsChanged(10, 70, 0)
       reapingMachine.status should be(true)
     }
 
     it("water_machine_should_start_if_temperature_over_10_degree_and_humidity_less_than_55_and_wind_power_less_than_4") {
-      val weatherData = getWeatherData
+      val weatherData = new WeatherData(seedingMachine, reapingMachine, wateringMachine)
+      weatherData.subscribe(new WateringMachineObserver(wateringMachine))
       weatherData.measurementsChanged(12, 50, 2)
       wateringMachine.status should be(true)
     }
   }
 
-  def getWeatherData:WeatherData = {
-    new WeatherData(seedingMachine, reapingMachine, wateringMachine)
-  }
 }
