885fe3584fd57dd79dd0c83dc1a36308427518c7
johnjunzhang <johnjunzhang@gmail.com>
2014-07-08 16:53:17 +0800

    [Jun] add more tests

--- practices/src/test/scala/refactoringToObserverFacts/WeatherDataFact.scala
+++ practices/src/test/scala/refactoringToObserverFacts/WeatherDataFact.scala
@@ -17,6 +17,18 @@ class WeatherDataFact extends FunSpec with Matchers {
       weatherData.measurementsChanged(10, 0, 0)
       seedingMachine.status should be(true)
     }
+
+    it("reaping_machine_should_start_if_temperature_over_5_degree_and_humidity_over_65") {
+      val weatherData = getWeatherData
+      weatherData.measurementsChanged(10, 70, 0)
+      reapingMachine.status should be(true)
+    }
+
+    it("water_machine_should_start_if_temperature_over_10_degree_and_humidity_less_than_55_and_wind_power_less_than_4") {
+      val weatherData = getWeatherData
+      weatherData.measurementsChanged(12, 50, 2)
+      wateringMachine.status should be(true)
+    }
   }
 
   def getWeatherData:WeatherData = {
